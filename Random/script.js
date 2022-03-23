5cf4913c906dd4d715dfcc2164fb6aa274530529
zayona <alzain05@icloud.com>
2022-03-23 18:19:58 +0300

    done

--- Random/script.js
+++ Random/script.js
@@ -1,3 +1,18 @@
 // project أنشئ كائن
-
+const project = {
+  type: "string",
+  users: "100",
+  biography() {
+    return "this is my project";
+  },
+};
 // randomProperty أنشئ دالة
+
+function randomProperty(obj) {
+  const keys = Object.keys(obj);
+
+  const random = Math.floor(Math.random() * keys.length);
+  console.log(keys[random]);
+  console.log(obj["${keys[random]}"]);
+}
+randomProperty(project);
